d0038e0e6f9e1c1b750699be33330674a6edd6b3
dsjgm921 <102582915+dsjgm921@users.noreply.github.com>
2022-07-30 15:34:40 +0900

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,7 @@
+save_dir                    = '/content/gdrive/MyDrive/Colab Notebooks/FSL_Relationnet_custom_data/'
+data_dir = {}
+data_dir['CUB']             = './filelists/CUB/' 
+data_dir['miniImagenet']    = './filelists/miniImagenet/' 
+data_dir['omniglot']        = './filelists/omniglot/' 
+data_dir['emnist']          = './filelists/emnist/' 
+data_dir['custom']        = '/content/gdrive/MyDrive/Colab Notebooks/FSL_Relationnet_custom_data/custom_data/'
